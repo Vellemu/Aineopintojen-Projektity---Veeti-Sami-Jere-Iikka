507853e0b006c2bd27668bbbe00f5fb2f4a76038
JLuoma98 <jere.luoma@gmail.com>
2024-10-23 13:13:56 +0300

    Näkymä maan datalle siirretty omaksi komponentiksi

--- sahkokartta/src/App.jsx
+++ sahkokartta/src/App.jsx
@@ -1,11 +1,27 @@
+import { useState, useEffect } from 'react';
 import MapComponent from './components/MapComponent'
 import './sahkokartta.css'
+import CountryData from './components/CountryData';
 
 const App = () => {
+  const [selectedCountry, setSelectedCountry] = useState(null);
+
+  /**
+   * Päivittää valitun maan tiedot
+   */
+  useEffect(() => {
+    if (selectedCountry) {
+      console.log('Valittu maa päivittyy:', selectedCountry);
+    } 
+  },  [selectedCountry]);
+
   return (
     <>
       <h1>Sähkökartta</h1>
-      <MapComponent />
+      <MapComponent setSelectedCountry={setSelectedCountry}/>
+      {selectedCountry &&
+        <CountryData selectedCountry={selectedCountry} setSelectedCountry={setSelectedCountry}/>
+      }
     </>
   )
 }
