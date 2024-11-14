54439f04c9962afeb14cd581c83d3a25eebfbc56
iitaseil <iitaseil@jyu.fi>
2024-11-14 17:24:25 +0200

    areachart/slider

--- sahkokartta/src/components/PercentAreaChart.jsx
+++ sahkokartta/src/components/PercentAreaChart.jsx
@@ -4,14 +4,20 @@ import { fetchMonthlyDataByYear } from '../api';
 import { useState, useEffect } from 'react';
 import { Slider } from '@mui/material';
 
+/**
+ * TODO: Vuosittainen datan esitys kuukausittaisen lisäksi
+ * TODO: Värikoodatut tuotantotapojen tekstit näkyviin sivulle
+ */
 const PercentAreaChart = ({countryCode}) => {
   const [chartData, setChartData] = useState(null);
   const [sliderValue, setSliderValue] = useState(2023);
-  
+  const monthlyDataYears = [2017, 2018, 2019, 2020, 2021, 2022, 2023, 2024];
+
   useEffect(() => {
     /**
      * Haetaan valitun vuoden kuukausittainen sähköntuotantodata ja 
      * järjestetään se kuukausittain sen kaaviota varten. 
+     * TODO: järkevämpi/siistimpi totetus for-silmukkujen sijaan
      */
     const getMonthlyData = async () => { 
       const promise = fetchMonthlyDataByYear(countryCode, sliderValue); 
@@ -37,11 +43,16 @@ const PercentAreaChart = ({countryCode}) => {
     setSliderValue(year);
   }
 
-  const getPercent = (value, total) => {
-    const ratio = total > 0 ? value / total : 0;
-    return (ratio * 100).toFixed(2);
+  const marks = (values) => {
+    var a = [];
+    values.map((x) =>  a.push({value: x, label: x.toString()}));
+    return a;
   };
       
+  /**
+   * Näyttää eri tuotantotapojen sähköntuotantomäärät 
+   * valitulta kuukaudelta kursorin ollessa kaavion päällä.
+   */
   const renderTooltipContent = (o) => {
     const { payload, label } = o;
     const total = payload.reduce((result, entry) => result + entry.value, 0).toFixed(2);
@@ -63,25 +74,30 @@ const PercentAreaChart = ({countryCode}) => {
     }
   }
 
-    return (
-      <div className="customized-tooltip-content">
-        <p className="total">{`${getFullName(label)}: (Total: ${total} TWh)`}</p>
-        <ul className="list">
-          {payload.map((entry, index) => (
-            <li key={`item-${index}`} style={{ color: entry.color }}>
-              {`${entry.name}: ${entry.value} TWh (${getPercent(entry.value, total)}%)`}
-            </li>
-          ))}
-        </ul>
-      </div>
-    );
+  const getPercent = (value, total) => {
+    const ratio = total > 0 ? value / total : 0;
+    return (ratio * 100).toFixed(2);
+  };
+
+  return (
+    <div className="customized-tooltip-content">
+      <p className="total">{`${getFullName(label)}: (Total: ${total} TWh)`}</p>
+      <ul className="list">
+        {payload.map((entry, index) => (
+          <li key={`item-${index}`} style={{ color: entry.color }}>
+            {`${entry.name}: ${entry.value} TWh (${getPercent(entry.value, total)}%)`}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
   };
     
   return (
     <>
     <div className='areachart-container'>
       {chartData && 
-      <AreaChart width={900} height={400} className='areachart'
+      <AreaChart width={800} height={400} className='areachart'
           data={chartData}
           stackOffset='expand'
           margin={{
@@ -98,23 +114,21 @@ const PercentAreaChart = ({countryCode}) => {
           <Area type="monotone" dataKey="Coal" stackId="1" stroke="#000000" fill="#000000" />
           <Area type="monotone" dataKey="Gas" stackId="1" stroke="#ff00ff" fill="#ff00ff" />
           <Area type="monotone" dataKey="Hydro" stackId="1" stroke="#00d4ff" fill="#00d4ff" />
-          {/* <Area type="monotone" dataKey="Net imports" stackId="1" stroke="#800000" fill="#800000" /> */}
           <Area type="monotone" dataKey="Nuclear" stackId="1" stroke="#cccc00" fill="#dddd00" />
           <Area type="monotone" dataKey="Other fossil" stackId="1" stroke="#808080" fill="#808080" />
           <Area type="monotone" dataKey="Other renewables" stackId="1" stroke="#a020f0" fill="#a020f0" />
           <Area type="monotone" dataKey="Solar" stackId="1" stroke="#ff0000" fill="#ff0000" />
           <Area type="monotone" dataKey="Wind" stackId="1" stroke="#ffffff" fill="#eeeeee" />
       </AreaChart>}
-      <Slider className='slider'
-        defaultValue={2023}
-        onChange={setYear}
-        step={1}
-        marks={true}
-        min={2017}
-        max={2024}
-        valueLabelDisplay="on"
-        size='small'
-      />
+        <Slider className='slider'
+          step={null}
+          marks={marks(monthlyDataYears)}
+          defaultValue={2023}
+          min={2017}
+          max={2024}
+          valueLabelDisplay="on"
+          onChange={setYear}    
+        />  
     </div>
     </>    
   )  
